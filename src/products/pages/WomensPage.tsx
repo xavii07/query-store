eea55411ec3fe52147e66c31e5bb7077455ae88c
xavii07 <xaviergaspa123@gmail.com>
2023-11-23 23:25:05 -0500

    feat config react-query

--- src/products/pages/WomensPage.tsx
+++ src/products/pages/WomensPage.tsx
@@ -1,11 +1,18 @@
 import { ProductList } from "..";
+import { useProducts } from "../hooks/useProducts";
 
 export const WomensPage = () => {
+  const { products, isLoading } = useProducts({
+    filterKey: "women's clothing",
+  });
+
   return (
     <div className="flex-col">
       <h1 className="text-2xl font-bold">Productos para mujeres</h1>
 
-      <ProductList />
+      {isLoading && <p>Loading....</p>}
+
+      <ProductList products={products} />
     </div>
   );
 };
