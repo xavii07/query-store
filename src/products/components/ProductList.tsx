c83ab7c5e2ff20d8bd8b09c86134095d2529f472
xavii07 <xaviergaspa123@gmail.com>
2023-11-26 20:54:15 -0500

    feat prefetchProduct to pass mouse hover card

--- src/products/components/ProductList.tsx
+++ src/products/components/ProductList.tsx
@@ -1,15 +1,22 @@
 import { ProductCard } from "..";
 import { Product } from "../../interfaces/product";
+import usePrefetchProduct from "../hooks/usePrefetchProduct";
 
 interface PropsProductList {
   products: Product[];
 }
 
 export const ProductList = ({ products }: PropsProductList) => {
+  const prefetchProduct = usePrefetchProduct();
+
   return (
     <div className="mt-2 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-2 justify-center max-w-max">
       {products?.map((product) => (
-        <ProductCard key={product.id} product={product} />
+        <ProductCard
+          key={product.id}
+          product={product}
+          prefetchProduct={prefetchProduct.prefetchProduct}
+        />
       ))}
     </div>
   );
