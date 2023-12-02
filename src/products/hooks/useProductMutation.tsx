f1e3750de00f432e2fb35aa8990b0e9f2aab01f4
xavii07 <xaviergaspa123@gmail.com>
2023-12-01 22:09:40 -0500

    feat optimistic success

--- src/products/hooks/useProductMutation.tsx
+++ src/products/hooks/useProductMutation.tsx
@@ -14,13 +14,42 @@ const useProductMutation = () => {
       });
     },*/
 
+    onMutate: (product) => {
+      console.log("onMutate", product);
+      //todo: crear un producto optimista
+      const optimisticProduct = { id: Math.random(), ...product };
+
+      //alacenar el producto optimista en la cache
+      queryClient.setQueryData<Product[]>(
+        ["products", { filterKey: product.category }],
+        (old) => {
+          if (!old) return [optimisticProduct];
+          return [...old, optimisticProduct];
+        }
+      );
+
+      //retornar el producto optimista para que se muestre en la lista context
+      return { optimisticProduct };
+    },
+
     //TODO: agregar al final de la lista
-    onSuccess: (data) => {
+    onSuccess: (data, variables, context) => {
+      console.log({ data, variables, context });
+
+      //TODO: remover el producto optimista de la cache
+      queryClient.removeQueries({
+        queryKey: ["products", { filterKey: context?.optimisticProduct.id }],
+      });
+
       queryClient.setQueryData<Product[]>(
         ["products", { filterKey: data.category }],
         (old) => {
           if (!old) return [data];
-          return [...old, data];
+          return old.map((cacheProduct) => {
+            return cacheProduct.id === context?.optimisticProduct.id
+              ? data
+              : cacheProduct;
+          });
         }
       );
     },
